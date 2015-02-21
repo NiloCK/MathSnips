0a43f44b280c3c6b52d25336f09012a7f6f97461
NiloCK <kolinkennedy@gmail.com>
2015-02-21 14:46:29 -0330

    Cutoff problem at tops and sides solved. Adjacent fractions no longer touch. Set radius to .9 times half the canvas - it may be useful for this to be parametrizable in future

--- Fraction.js
+++ Fraction.js
@@ -14,15 +14,14 @@ Fraction.prototype.getCanvasDrawing = function (size) {
         console.log("Fraction defaulting to 100 px");
         size = 100;
     }
-    // todo: the 'cutoff' effect at the tops and sides is worse as the image shrinks. Fix it.
-
+    
     var canvas = document.createElement('canvas');
     canvas.height = size;
     canvas.width = size;
     var con = canvas.getContext('2d');
 
     var mid = size / 2;
-    var radius = mid * 0.95;
+    var radius = mid * 0.9;
     var angle = 2 * Math.PI / this.den;
 
     con.fillStyle = 'orange';
@@ -30,7 +29,7 @@ Fraction.prototype.getCanvasDrawing = function (size) {
     // the 'arc' fills
     for (var i = 0; i < this.num; i++) {
         con.beginPath();
-        con.arc(mid, mid, mid, angle * i, angle * (i + 1));
+        con.arc(mid, mid, radius, angle * i, angle * (i + 1));
         con.fill();
     }
 
@@ -38,8 +37,8 @@ Fraction.prototype.getCanvasDrawing = function (size) {
     con.beginPath();
     con.moveTo(mid, mid);
     for (var i = 0; i <= this.num; i++) {
-        con.lineTo(mid + mid * Math.cos(angle * i),
-                   mid + mid * Math.sin(angle * i));
+        con.lineTo(mid + radius * Math.cos(angle * i),
+                   mid + radius * Math.sin(angle * i));
     }
     con.moveTo(mid, mid);
     con.fill();
@@ -50,14 +49,15 @@ Fraction.prototype.getCanvasDrawing = function (size) {
         for (var i = 0; i < this.den; i++) {
             con.beginPath();
             con.moveTo(mid, mid);
-            con.lineTo(mid + mid * Math.cos(i * angle), mid + mid * Math.sin(i * angle));
+            con.lineTo(mid + radius * Math.cos(i * angle),
+                       mid + radius * Math.sin(i * angle));
             con.stroke();
         }
     }
 
     // the exterior circle
     con.beginPath();
-    con.arc(mid, mid, mid, 0, 2 * Math.PI);
+    con.arc(mid, mid, radius, 0, 2 * Math.PI);
     con.stroke();
 
     return canvas;
